945cfd897a6a0b273354e6c3b329a3bf7f515ccc
Thorsten Maier <Thorsten.Maier@oio.de>
2020-03-30 07:24:54 +0200

    Cross Origin

--- src/main/java/de/awr/StationApplication.java
+++ src/main/java/de/awr/StationApplication.java
@@ -2,8 +2,10 @@ package de.awr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin("http://localhost:4200")
 public class StationApplication {
 
 	public static void main(String[] args) {
